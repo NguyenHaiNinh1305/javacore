5916df925a924b551b1f0147e3de07079f8bcac0
nguyenhaininh <hulkhulk1245@gmai.com>
2022-06-03 00:35:12 +0700

    commit

--- src/com/company/entity/TypeSubject.java
+++
@@ -1,8 +0,0 @@
-package com.company.entity;
-
-import java.io.Serializable;
-
-public enum TypeSubject implements Serializable{
-
-    generalSubject, basicSubject, specializedSubject
-}
--- src/com/company/utils/file/DataWriteable.java
+++
@@ -1,7 +0,0 @@
-package com.company.utils.file;
-
-import java.io.FileNotFoundException;
-
-public interface DataWriteable {
-    void  writeDataToFile(Object[] objects, String pathFile) throws FileNotFoundException;
-}
